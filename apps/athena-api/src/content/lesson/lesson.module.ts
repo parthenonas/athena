a12be887df80f4a76cff107347d2a53c47ec2b50
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2025-12-04 09:04:25 +0300

    fix: dependencies

--- apps/athena-api/src/content/lesson/lesson.module.ts
+++ apps/athena-api/src/content/lesson/lesson.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { IdentityModule } from "../../identity";
 import { Lesson } from "./entities/lesson.entity";
 import { LessonService } from "./lesson.service";
 import { Block } from "../block/entities/block.entity";
 import { Course } from "../course/entities/course.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Lesson, Course, Block])],
+  imports: [TypeOrmModule.forFeature([Lesson, Course, Block]), IdentityModule],
   providers: [LessonService],
   exports: [LessonService],
 })
