7165007ec6dc66717d81aef1a7dd60cc34e46df8
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2025-12-08 00:51:45 +0300

    feat: block controller and tests

--- apps/athena-api/src/content/block/block.module.ts
+++ apps/athena-api/src/content/block/block.module.ts
@@ -1,14 +1,17 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { BlockController } from "./block.controller";
 import { BlockService } from "./block.service";
 import { IdentityModule } from "../../identity";
 import { Block } from "./entities/block.entity";
 import { Lesson } from "../lesson/entities/lesson.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Block, Lesson]), IdentityModule],
+  imports: [TypeOrmModule.forFeature([Block, Lesson]), IdentityModule, JwtModule],
   providers: [BlockService],
+  controllers: [BlockController],
   exports: [BlockService],
 })
 export class BlockModule {}
